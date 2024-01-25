4ef809b5b6a3ab7144e1a76e0a78f997cd649d12
PalmDevs <PalmDevelops@gmail.com>
2024-01-26 01:57:07 +0700

    refactor(components/NavRail): make rendering logic cleaner

--- src/components/NavRail.tsx
+++ src/components/NavRail.tsx
@@ -2,27 +2,22 @@ import type { Component, ComponentProps, JSX } from 'solid-js'
 
 import styles from './NavRail.module.scss'
 
-const NavRail: Component<{ children?: JSX.Element }> = (props) => {
-    return (
-        <nav class={styles.NavRail}>
-            {props.children}
-        </nav>
-    )
+const NavRail: Component<{ children?: JSX.Element }> = props => {
+    return <nav class={styles.NavRail}>{props.children}</nav>
 }
 
-const NavRailButton: Component<NavRailItemProps> = (props) => {
+const NavRailButton: Component<NavRailItemProps> = props => {
     return (
         <button class={styles.NavRailItem} role="button" onClick={props.onClick}>
             <div class={styles.NavRailItemIconCont}>
-                {
-                    props.altIcon &&
-                    <props.altIcon
-                        data-icon-non-default
-                    />
-                }
-                <props.icon
-                    {...props.altIcon && { ['data-icon-default']: true }}
-                />
+                {props.altIcon ? (
+                    <>
+                        <props.altIcon data-icon-non-default />
+                        <props.icon data-icon-default />
+                    </>
+                ) : (
+                    <props.icon />
+                )}
             </div>
             <label>{props.label}</label>
         </button>
@@ -32,11 +27,11 @@ const NavRailButton: Component<NavRailItemProps> = (props) => {
 export { NavRail, NavRailButton }
 
 type NavRailItemProps = {
-    icon: IconType,
-    altIcon?: IconType,
-    label: string,
+    icon: IconType
+    altIcon?: IconType
+    label: string
 
-    onClick?: () => void,
+    onClick?: () => void
 }
 
 type IconType = Component<ComponentProps<'svg'>>
