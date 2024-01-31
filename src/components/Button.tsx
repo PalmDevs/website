f0b6614e88cf85e941cf023c98f4693d1b90ef52
PalmDevs <PalmDevelops@gmail.com>
2024-02-01 00:11:38 +0700

    feat(components): add buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,69 @@
+import { type Component, type ComponentProps } from 'solid-js'
+import type { IconType } from '.'
+
+import { undefinedIf } from '../utils'
+
+import styles from './Button.module.scss'
+
+const Button: Component<ButtonProps> = props => {
+    return (
+        <button
+            {...props}
+            onClick={undefinedIf(!props.disabled, props.onClick)}
+            {...getBasicAttributesFromProps(props)}
+        >
+            {renderIconIfSpecified(props.leadingIcon)}
+            {props.children}
+            {renderIconIfSpecified(props.trailingIcon)}
+        </button>
+    )
+}
+
+const LinkButton: Component<LinkButtonProps> = props => {
+    return (
+        <a
+            {...props}
+            href={undefinedIf(!props.disabled, props.href)}
+            target={props.openInCurrentTab ? '_self' : '_blank'}
+            {...getBasicAttributesFromProps(props)}
+        >
+            {renderIconIfSpecified(props.leadingIcon)}
+            {props.children}
+            {renderIconIfSpecified(props.trailingIcon)}
+        </a>
+    )
+}
+
+function getBasicAttributesFromProps(props: ButtonProps | LinkButtonProps) {
+    const disabledProp = undefinedIf(props.disabled, true)
+
+    return {
+        class: `${styles.BaseButton} ${styles[props.variant ?? 'primary']} ${props.class ?? ''}`,
+        tabIndex: undefinedIf(props.disabled, -1),
+        disabled: disabledProp,
+        ['aria-disabled']: disabledProp,
+    } as const satisfies ComponentProps<'button'> & ComponentProps<'a'>
+}
+
+function renderIconIfSpecified(Icon: IconType | undefined) {
+    return Icon ? <Icon class={styles.ButtonIcon} /> : null
+}
+
+type BaseButtonProps = {
+    variant?: 'primary' | 'secondary' | 'tertiary'
+    children: string | string[]
+
+    leadingIcon?: IconType
+    trailingIcon?: IconType
+
+    disabled?: boolean
+}
+
+type LinkButtonProps = ComponentProps<'a'> & BaseButtonProps & {
+    href: string
+    openInCurrentTab?: boolean
+}
+
+type ButtonProps = ComponentProps<'button'> & BaseButtonProps
+
+export { Button, LinkButton }
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import type { Component, ComponentProps } from 'solid-js'
+export type IconType = Component<ComponentProps<'svg'>>
+
+export * from './Button'
+export * from './NavRail'
