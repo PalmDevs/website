e8df39d10173bc02fc91b83626a6af6779e78d4f
PalmDevs <PalmDevelops@gmail.com>
2024-02-01 00:07:02 +0700

    feat!: migrate workspace root to solid start

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
-import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
-import solidSvg from 'vite-plugin-solid-svg'
-// import devtools from 'solid-devtools/vite'
+import { defineConfig } from '@solidjs/start/config'
+import svgPlugin from 'vite-plugin-solid-svg'
 
 export default defineConfig({
     plugins: [
-        /* 
-            Uncomment the following line to enable solid-devtools.
-            For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-        */
-        // devtools(),
-        solidPlugin({
-            ssr: true,
-        }),
-        solidSvg({
+        svgPlugin({
             defaultAsComponent: true,
         }),
     ],
-    server: {
-        port: 3000,
-    },
-    build: {
-        target: 'esnext',
-    },
 })
