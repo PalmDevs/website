e6b7f72e844cac8b3ff9e142a48a122d9431e05d
PalmDevs <PalmDevelops@gmail.com>
2024-02-01 00:10:08 +0700

    feat: add constants

---
+++ src/constants/socials.ts
@@ -0,0 +1,12 @@
+export default {
+    github: {
+        title: 'Github',
+        icon: 'github',
+        url: 'https://github.com/PalmDevs',
+    },
+    email: {
+        title: 'Email',
+        icon: null,
+        url: 'mailto:PalmDevelops+Web@gmail.com'
+    },
+} as const satisfies Record<string, { title: string; icon: string | null; url: string; }>
