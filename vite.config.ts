496f8a2f19d13ece7dfcabbfc7958fc772652349
PalmDevs <PalmDevelops@gmail.com>
2024-02-04 15:57:41 +0700

    feat: prerender index page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from '@solidjs/start/config'
 import svgPlugin from 'vite-plugin-solid-svg'
 
 export default defineConfig({
+    experimental: {
+        hmrPartialAccept: true,
+    },
+    start: {
+        ssr: true,
+        // islands: true,
+        server: {
+            prerender: {
+                routes: ['/'],
+            },
+        },
+    },
     plugins: [
         svgPlugin({
             defaultAsComponent: true,
