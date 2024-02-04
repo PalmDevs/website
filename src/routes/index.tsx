827052991066677ba4742fea8d3febff1c38636d
PalmDevs <PalmDevelops@gmail.com>
2024-02-04 16:00:59 +0700

    fix(routes): change component imports and usage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import appStyles from '~/app.module.scss'
 
 import socials from '~/constants/socials'
 
-import { Button, LinkButton, Page, NavigationSkipTarget } from '~/components'
+import { Button, LinkButton, Content, NavigationSkipTarget, Section } from '~/components'
 import { resolveIcon } from '~/utils'
 
 import IconNext from '~/assets/icons/button/next.svg'
@@ -13,39 +13,35 @@ import IconGitHub from '~/assets/icons/socials/github.svg'
 
 const IndexPage: Component = () => {
     return (
-        <Page>
-            <section class={styles.Section}>
-                <div
-                    class={`${styles.SectionContent} ${styles.SectionHeroContent}`}
-                >
-                    <div class={styles.SectionTextContent}>
-                        <h1>
-                            Hey there, I'm{' '}
-                            <span class={appStyles.GradientText}>Palm</span>!
-                        </h1>
-                        <h2>I design and build cool things.</h2>
-                        <p>
-                            I'm a 15-year-old self-taught full-stack developer
-                            and a UI/UX designer who is interested in security
-                            and automation.
-                        </p>
-                    </div>
-                    <div class={styles.SectionButtons}>
-                        <NavigationSkipTarget>
-                            <Button>Explore projects</Button>
-                        </NavigationSkipTarget>
-                        <LinkButton
-                            leadingIcon={resolveIcon(IconGitHub)}
-                            trailingIcon={resolveIcon(IconNext)}
-                            variant="tertiary"
-                            href={socials.github.url}
-                        >
-                            GitHub profile
-                        </LinkButton>
-                    </div>
+        <Content>
+            <Section type='large'>
+                <div class={styles.SectionTextContent}>
+                    <h1>
+                        Hey there, I'm{' '}
+                        <span class={appStyles.GradientText}>Palm</span>!
+                    </h1>
+                    <h2>I design and build cool things.</h2>
+                    <p>
+                        I'm a 15-year-old self-taught full-stack developer
+                        and a UI/UX designer who is interested in security
+                        and automation.
+                    </p>
                 </div>
-            </section>
-            <section class={styles.Section}>
+                <div class={styles.SectionButtons}>
+                    <NavigationSkipTarget>
+                        <Button>Explore projects</Button>
+                    </NavigationSkipTarget>
+                    <LinkButton
+                        leadingIcon={resolveIcon(IconGitHub)}
+                        trailingIcon={resolveIcon(IconNext)}
+                        variant="tertiary"
+                        href={socials.github.url}
+                    >
+                        GitHub profile
+                    </LinkButton>
+                </div>
+            </Section>
+            <Section type='large'>
                 <div class={styles.SectionTextContent}>
                     <h1 class="headline-title">My Projects</h1>
                     <p>
@@ -53,8 +49,8 @@ const IndexPage: Component = () => {
                         to me.
                     </p>
                 </div>
-            </section>
-        </Page>
+            </Section>
+        </Content>
     )
 }
 
--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -3,7 +3,14 @@ import { createSignal, type Component, createEffect } from 'solid-js'
 import styles from '~/components/Page.module.scss'
 import appStyles from '~/app.module.scss'
 
-import { Button, LinkButton, Page, NavigationSkipTarget, IconType } from '~/components'
+import {
+    Button,
+    LinkButton,
+    NavigationSkipTarget,
+    IconType,
+    Section,
+    Content,
+} from '~/components'
 import { resolveIcon } from '~/utils'
 
 import IconNext from '~/assets/icons/button/next.svg'
@@ -12,8 +19,10 @@ import { createStore } from 'solid-js/store'
 
 const TestPage: Component = () => {
     const [buttonsDisabled, setButtonsDisabled] = createSignal(false)
-    const [buttonsLeadingIconShown, setButtonsLeadingIconShown] = createSignal(false)
-    const [buttonsTrailingIconShown, setButtonsTrailingIconShown] = createSignal(false)
+    const [buttonsLeadingIconShown, setButtonsLeadingIconShown] =
+        createSignal(false)
+    const [buttonsTrailingIconShown, setButtonsTrailingIconShown] =
+        createSignal(false)
     // const [showNavRail, setShowNavRail] = createSignal(false)
 
     const [buttonProps, setButtonProps] = createStore<{
@@ -29,105 +38,106 @@ const TestPage: Component = () => {
     createEffect(() => {
         setButtonProps({
             disabled: buttonsDisabled(),
-            leadingIcon: buttonsLeadingIconShown() ? resolveIcon(IconWavingHandFilled) : undefined,
-            trailingIcon: buttonsTrailingIconShown() ? resolveIcon(IconNext) : undefined,
+            leadingIcon: buttonsLeadingIconShown()
+                ? resolveIcon(IconWavingHandFilled)
+                : undefined,
+            trailingIcon: buttonsTrailingIconShown()
+                ? resolveIcon(IconNext)
+                : undefined,
         })
-    }, [buttonsDisabled(), buttonsLeadingIconShown(), buttonsTrailingIconShown()])
+    }, [
+        buttonsDisabled(),
+        buttonsLeadingIconShown(),
+        buttonsTrailingIconShown(),
+    ])
 
     return (
-        <Page>
-            <section class={styles.Section}>
-                <div
-                    class={`${styles.SectionContent} ${styles.SectionHeroContent}`}
-                >
-                    <div class={styles.SectionTextContent}>
-                        <h1>
-                            Design system{' '}
-                            <span class={appStyles.GradientText}>testing</span>
-                        </h1>
-                        <h2>Welcome to the test area</h2>
-                        <p>
-                            This is where you can play and test with the
-                            components from the design system.
-                            <br />
-                            The design system is inspired by both Material
-                            Design and Fluent UI (with a bit of fun animations
-                            and 💖 added on top).
-                        </p>
-                    </div>
+        <Content>
+            <Section type="large">
+                <div class={styles.SectionTextContent}>
+                    <h1>
+                        Design system{' '}
+                        <span class={appStyles.GradientText}>testing</span>
+                    </h1>
+                    <h2>Welcome to the test area</h2>
+                    <p>
+                        This is where you can play and test with the components
+                        from the design system.
+                        <br />
+                        The design system is inspired by both Material Design
+                        and Fluent UI (with a bit of fun animations and 💖 added
+                        on top).
+                    </p>
                 </div>
-            </section>
-            <section class={styles.Section}>
-                <div
-                    class={`${styles.SectionContent} ${styles.SectionHeroContent}`}
-                >
-                    <div class={styles.SectionTextContent}>
-                        <h1 class="headline-title">Buttons</h1>
-                        <p>
-                            Buttons are interactive elements that users can tap
-                            or click on to do an action.
-                            <br />
-                            <br />
-                            By default when skipping navigation, buttons should
-                            be focused first. To do this, wrap the button in a{' '}
-                            <code>{'<NavigationSkipTarget>'}</code> component.{' '}
-                            <strong>
-                                If you do not do so, a warning will be logged in
-                                the console.{' '}
-                            </strong>
-                            Click on the navigation rail and press{' '}
-                            <kbd>Tab</kbd> to test.
-                        </p>
-                    </div>
-                    <div class={styles.SectionButtons}>
-                        <Button
-                            {...buttonProps}
-                            onClick={() => alert('You clicked!')}
-                        >
-                            Primary button
-                        </Button>
-                        <NavigationSkipTarget>
-                            <Button
-                                {...buttonProps}
-                                variant="secondary"
-                            >
-                                Secondary button (nav skip target)
-                            </Button>
-                        </NavigationSkipTarget>
-                        <LinkButton
-                            {...buttonProps}
-                            variant="tertiary"
-                            href="https://youtube.com/watch?v=dQw4w9WgXcQ"
-                        >
-                            Tertiary button (link)
-                        </LinkButton>
-                    </div>
-                    <div class={styles.SectionButtons}>
+            </Section>
+            <Section type="large">
+                <div class={styles.SectionTextContent}>
+                    <h1 class="headline-title">Buttons</h1>
+                    <p>
+                        Buttons are interactive elements that users can tap or
+                        click on to do an action.
+                        <br />
+                        <br />
+                        By default when skipping navigation, buttons should be
+                        focused first. To do this, wrap the button in a{' '}
+                        <code>{'<NavigationSkipTarget>'}</code> component.{' '}
+                        <strong>
+                            If you do not do so, a warning will be logged in the
+                            console.{' '}
+                        </strong>
+                        Click on the navigation rail and press <kbd>Tab</kbd> to
+                        test.
+                    </p>
+                </div>
+                <div class={styles.SectionButtons}>
                     <Button
-                            onClick={() =>
-                                setButtonsDisabled(!buttonsDisabled())
-                            }
-                        >
-                            Disabled: {buttonsDisabled() ? 'yes' : 'no'}
+                        {...buttonProps}
+                        onClick={() => alert('You clicked!')}
+                    >
+                        Primary button
+                    </Button>
+                    <NavigationSkipTarget>
+                        <Button {...buttonProps} variant="secondary">
+                            Secondary button (nav skip target)
                         </Button>
-                        <Button
-                            onClick={() =>
-                                setButtonsLeadingIconShown(!buttonsLeadingIconShown())
-                            }
-                        >
-                            Leading icons: {buttonsLeadingIconShown() ? 'yes' : 'no'}
-                        </Button>
-                        <Button
-                            onClick={() =>
-                                setButtonsTrailingIconShown(!buttonsTrailingIconShown())
-                            }
-                        >
-                            Trailing icons: {buttonsTrailingIconShown() ? 'yes' : 'no'}
-                        </Button>
-                    </div>
+                    </NavigationSkipTarget>
+                    <LinkButton
+                        {...buttonProps}
+                        variant="tertiary"
+                        href="https://youtube.com/watch?v=dQw4w9WgXcQ"
+                    >
+                        Tertiary button (link)
+                    </LinkButton>
                 </div>
-            </section>
-        </Page>
+                <div class={styles.SectionButtons}>
+                    <Button
+                        onClick={() => setButtonsDisabled(!buttonsDisabled())}
+                    >
+                        Disabled: {buttonsDisabled() ? 'yes' : 'no'}
+                    </Button>
+                    <Button
+                        onClick={() =>
+                            setButtonsLeadingIconShown(
+                                !buttonsLeadingIconShown(),
+                            )
+                        }
+                    >
+                        Leading icons:{' '}
+                        {buttonsLeadingIconShown() ? 'yes' : 'no'}
+                    </Button>
+                    <Button
+                        onClick={() =>
+                            setButtonsTrailingIconShown(
+                                !buttonsTrailingIconShown(),
+                            )
+                        }
+                    >
+                        Trailing icons:{' '}
+                        {buttonsTrailingIconShown() ? 'yes' : 'no'}
+                    </Button>
+                </div>
+            </Section>
+        </Content>
     )
 }
 
