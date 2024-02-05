4381186704e9c1bac58e6a0564c662d4c415ac3b
PalmDevs <PalmDevelops@gmail.com>
2024-02-06 00:13:56 +0700

    feat(routes/index): add handler and icon to hero's explore button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { type Component } from 'solid-js'
+import { clientOnly } from '@solidjs/start'
 
 import styles from '~/components/Page.module.scss'
 import appStyles from '~/app.module.scss'
@@ -10,26 +11,37 @@ import { resolveIcon } from '~/utils'
 
 import IconNext from '~/assets/icons/button/next.svg'
 import IconGitHub from '~/assets/icons/socials/github.svg'
+import IconProjectsFilled from '~/assets/icons/nav/all_inbox_filled.svg'
 
 const IndexPage: Component = () => {
     return (
         <Content>
-            <Section type='large'>
+            <Section id="hero" type="large">
                 <div class={styles.SectionTextContent}>
                     <h1>
-                        Hey there, I'm{' '}
-                        <span class={appStyles.GradientText}>Palm</span>!
+                        Hey there, I'm <span class={appStyles.GradientText}>Palm</span>!
                     </h1>
                     <h2>I design and build cool things.</h2>
                     <p>
-                        I'm a 15-year-old self-taught full-stack developer
-                        and a UI/UX designer who is interested in security
-                        and automation.
+                        I'm a 15-year-old self-taught full-stack developer and a UI/UX
+                        designer who is interested in security and automation.
                     </p>
                 </div>
                 <div class={styles.SectionButtons}>
                     <NavigationSkipTarget>
-                        <Button>Explore projects</Button>
+                        <Button
+                            onClick={() =>
+                                document
+                                    .getElementById('projects')
+                                    ?.scrollIntoView({
+                                        behavior: 'smooth',
+                                        block: 'nearest',
+                                    })
+                            }
+                            leadingIcon={resolveIcon(IconProjectsFilled)}
+                        >
+                            Explore projects
+                        </Button>
                     </NavigationSkipTarget>
                     <LinkButton
                         leadingIcon={resolveIcon(IconGitHub)}
@@ -41,13 +53,10 @@ const IndexPage: Component = () => {
                     </LinkButton>
                 </div>
             </Section>
-            <Section type='large'>
+            <Section id="projects">
                 <div class={styles.SectionTextContent}>
                     <h1 class="headline-title">My Projects</h1>
-                    <p>
-                        Things I've contributed to or built that are meaningful
-                        to me.
-                    </p>
+                    <p>Things I've contributed to or built that are meaningful to me.</p>
                 </div>
             </Section>
         </Content>
