c87d2fbf634c4cda7f8a2aae85432d4788729c79
PalmDevs <PalmDevelops@gmail.com>
2024-03-01 02:46:39 +0700

    feat(constants): add `projects` constants

---
+++ src/constants/projects.ts
@@ -0,0 +1,13 @@
+import type { ProjectCardProps } from '~/components/content/ProjectCard'
+
+export default [
+    {
+        name: 'ReVanced',
+        description:
+            'ReVanced is a patcher used to modify Android applications. It also allows long-lasting patches to be created with little maintainance.',
+        level: 'Organization member',
+        jobs: ['UI/UX', 'CI', 'Bots'],
+        href: 'https://revanced.app',
+        image: '/assets/projects/revanced.svg',
+    },
+] as const satisfies Array<ProjectCardProps>
