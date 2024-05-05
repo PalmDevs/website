9d585ff32d8ac044c81ebef7d2b23a5a2e1ba220
PalmDevs <git@palmdevs.me>
2024-05-05 22:31:36 +0700

    fix: fix issues with post rendering

--- src/routes/(layout)/blog/[...post].tsx
+++ src/routes/(layout)/blog/[...post].tsx
@@ -1,4 +1,5 @@
-import { type Component, Show, createResource, createSignal, createEffect, onCleanup } from 'solid-js'
+import { type Component, Show, createResource, createSignal, onCleanup, createEffect } from 'solid-js'
+import { Dynamic } from 'solid-js/web'
 import { Title } from '@solidjs/meta'
 import { useParams } from '@solidjs/router'
 
@@ -7,20 +8,23 @@ import { SolidMarkdown } from 'solid-markdown'
 import { readFile } from 'fs/promises'
 
 import { Column, Divider, Page, Section } from '~/components'
+import { logger } from '~/utils'
 import FourOhFourPage from '~/routes/(layout)/[...404]'
 
 import styles from './[...post].module.scss'
-import { Dynamic } from 'solid-js/web'
-import { logger } from '~/utils'
 
 export default (() => {
     const params = useParams<{ post: string }>()
-    const [maybePost] = createResource(async () => fetchPost(params.post))
+    const [maybePost] = createResource(params.post, fetchPost, {
+        deferStream: true,
+        onHydrated: () => logger.log('Post loaded'),
+    })
 
     const [timePosted, setTimePosted] = createSignal('...')
 
     createEffect(() => {
         const post = maybePost()
+
         if (post) {
             const epochPosted = post.updated ?? post.published
             setTimePosted(formatTime(epochPosted))
@@ -49,6 +53,7 @@ export default (() => {
                             <Divider />
                         </Column>
                         <SolidMarkdown
+                            renderingStrategy="reconcile"
                             components={{
                                 h1: HeadingRenderer,
                                 h2: HeadingRenderer,
@@ -68,18 +73,6 @@ export default (() => {
     )
 }) satisfies Component
 
-const HeadingRenderer = (props: { children: Component[]; level: number }) => (
-    <Dynamic component={`h${props.level}`} id={contentToId(props.children.toString())}>
-        {props.children}
-    </Dynamic>
-)
-
-const contentToId = (content: string) =>
-    content
-        .toLowerCase()
-        .replaceAll(/[!@#$%^&*()=+\[\]{}\\|:;'"<>,.?/]/g, '')
-        .replaceAll(/\s/g, '-')
-
 type Post = {
     title: string
     description: string
@@ -87,6 +80,19 @@ type Post = {
     updated?: number
     text: string
 }
+
+const HeadingRenderer = (props: { children: Component[]; level: number }) => (
+    <Dynamic
+        component={`h${props.level}`}
+        id={props.children
+            .toString()
+            .toLowerCase()
+            .replaceAll(/[!@#$%^&*()=+\[\]{}\\|:;'"<>,.?/]/g, '')
+            .replaceAll(/\s/g, '-')}
+    >
+        {props.children}
+    </Dynamic>
+)
 
 const fetchPost = async (post: string): Promise<Post | null> => {
     'use server'
@@ -96,7 +102,7 @@ const fetchPost = async (post: string): Promise<Post | null> => {
         const text = await readFile(`posts/${post}/content.md`, 'utf8')
 
         if (!metadata || !text) return null
-        
+
         return {
             ...metadata,
             text,
