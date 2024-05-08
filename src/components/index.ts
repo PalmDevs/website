c35b8b49f4728915c46135155c19f2dfc4a55cde
PalmDevs <git@palmdevs.me>
2024-05-09 02:25:32 +0700

    feat(components): reexport from `effects`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import type { Component, ComponentProps } from 'solid-js'
 
 export * from './buttons'
+export * from './effects'
 export * from './Page'
 export { default as NavDock } from './NavDock'
 export { default as ProjectCard } from './ProjectCard'
