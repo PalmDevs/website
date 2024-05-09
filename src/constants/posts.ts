8ef0107ec45ec4910942492cff6faa7e9e460884
PalmDevs <git@palmdevs.me>
2024-05-10 04:50:25 +0700

    fix(constants/posts): broken transformer regex

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -2,7 +2,7 @@ import type { Component } from 'solid-js'
 
 const posts = import.meta.glob<false, string, Post>('../../posts/*.mdx')
 const Posts: Record<string, () => Promise<Post>> = Object.fromEntries(
-    await Promise.all(Object.entries(posts).map(([path, post]) => [path.match(/\/([a-zA-Z0-9-_])\.mdx/)![1], post])),
+    await Promise.all(Object.entries(posts).map(([path, post]) => [path.match(/\/([a-zA-Z0-9-_]+)\.mdx/)![1], post])),
 )
 
 export default Posts
