3a300dd7fe5a8c9c20f003fd598d17a658218678
PalmDevs <git@palmdevs.me>
2025-10-01 00:05:51 +0700

    fix: remove problematic integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import solid from '@astrojs/solid-js'
-import playformInline from '@playform/inline'
 import { defineConfig } from 'astro/config'
 import compressor from 'astro-compressor'
 import expressiveCode from 'astro-expressive-code'
@@ -11,7 +10,6 @@ import favicons from 'astro-favicons'
 // @ts-expect-error: No types
 import { astroImageTools as imageTools } from 'astro-imagetools'
 import metaTags from 'astro-meta-tags'
-import purgecss from 'astro-purgecss'
 import robotsTxt from 'astro-robots-txt'
 import sonda from 'sonda/astro'
 import UnoCSS from 'unocss/astro'
@@ -37,8 +35,6 @@ export default defineConfig({
 		expressiveCode(),
 		mdx(),
 		robotsTxt(),
-		playformInline(),
-		purgecss(),
 		metaTags(),
 		favicons(),
 		compressor(),
