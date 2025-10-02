eb25540fa2a531d28bb94b9ad2099338831c0ebe
PalmDevs <git@palmdevs.me>
2025-10-02 22:11:21 +0700

    feat: prepare skills assets for Work page

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,76 +2,109 @@ export const SKILLS = [
 	{
 		name: 'JavaScript',
 		icon: '/assets/skills/js.svg',
+		variant: 'large',
 	},
 	{
 		name: 'TypeScript',
 		icon: '/assets/skills/ts.svg',
+		variant: 'large',
 	},
 	{
 		name: 'React',
 		icon: '/assets/skills/react.svg',
+		variant: 'large',
 	},
 	{
 		name: 'Figma',
 		icon: '/assets/skills/figma.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'Git',
 		icon: '/assets/skills/git.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'SolidJS',
 		icon: '/assets/skills/solidjs.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'Bun',
 		icon: '/assets/skills/bun.svg',
+		variant: 'large-vert',
 	},
 	{
 		name: 'Node.js',
 		icon: '/assets/skills/nodejs.svg',
+		variant: 'large-vert',
 	},
 	{
 		name: 'HTML',
 		icon: '/assets/skills/html.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'CSS',
 		icon: '/assets/skills/css.svg',
+		variant: 'large-horz',
+	},
+	{
+		name: 'Astro',
+		icon: '/assets/skills/astro.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'Linux',
 		icon: '/assets/skills/linux.webp',
+		variant: 'large-horz',
 	},
 	{
 		name: 'Drizzle ORM',
 		icon: '/assets/skills/drizzle.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'Kotlin',
 		icon: '/assets/skills/kotlin.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'ElysiaJS',
 		icon: '/assets/skills/elysia.svg',
-	},
-	{
-		name: 'Python',
-		icon: '/assets/skills/python.svg',
+		variant: 'large-horz',
 	},
 	{
 		name: 'GitHub Actions',
 		icon: '/assets/skills/gha.svg',
+		variant: 'large-horz',
+	},
+	{
+		name: 'Semantic Release',
+		icon: '/assets/skills/semrel.svg',
+	},
+	{
+		name: 'Discord.js',
+		icon: '/assets/skills/djs.svg',
+	},
+	{
+		name: 'wit.ai',
+		icon: '/assets/skills/witai.svg',
+	},
+	{
+		name: 'Cloudflare',
+		icon: '/assets/skills/cloudflare.svg',
 	},
 	{
 		name: 'Docker',
 		icon: '/assets/skills/docker.svg',
 	},
 	{
-		name: 'Cloudflare',
-		icon: '/assets/skills/cloudflare.svg',
+		name: 'Python',
+		icon: '/assets/skills/python.svg',
 	},
-] as const satisfies Array<{
+] as Array<{
 	name: string
 	icon: string
+	variant?: 'large' | 'large-horz' | 'large-vert' | 'small'
 }>
