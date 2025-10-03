847d214dc1aca3ff4588981a648181d9810e699a
PalmDevs <git@palmdevs.me>
2025-10-03 17:42:39 +0700

    feat: light-dark experience images

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,8 +1,16 @@
 import { PROJECT_REVANCED, PROJECT_REVENGE } from './projects'
 
 const EXPERIENCES = [
-	{ ...PROJECT_REVANCED, image: '/assets/experience/revanced.webp' },
-	{ ...PROJECT_REVENGE, image: '/assets/experience/revenge.webp' },
+	{
+		...PROJECT_REVANCED,
+		image: '/assets/experience/revanced.webp',
+		imageLight: '/assets/experience/revanced-light.webp',
+	},
+	{
+		...PROJECT_REVENGE,
+		image: '/assets/experience/revenge.webp',
+		imageLight: '/assets/experience/revenge-light.webp',
+	},
 ] satisfies Experience[]
 
 export default EXPERIENCES
@@ -10,5 +18,6 @@ export default EXPERIENCES
 interface Experience {
 	name: string
 	image: string
+	imageLight?: string
 	href: string
 }
