91a7fd777a2b6ca38dd568390b6e45d6c050ee65
PalmDevs <git@palmdevs.me>
2025-11-16 00:42:07 +0700

    chore(data/projects): hide less important projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,7 @@
 import DataBackup from '~/images/projects/data_backup.svg'
-import GitBartender from '~/images/projects/git_bartender.webp'
-import Jasper from '~/images/projects/jasper.webp'
 import Other from '~/images/projects/other.svg'
 import ReVanced from '~/images/projects/revanced.svg'
 import Revenge from '~/images/projects/revenge.webp'
-import Website from '~/images/projects/website.svg'
 
 export const PROJECT_REVANCED: Project = {
 	name: 'ReVanced',
@@ -34,28 +31,28 @@ const PROJECTS = [
 		image: DataBackup,
 		hint: 'View repository',
 	},
-	{
-		name: 'My Website',
-		description: "You're on it right now. Thanks for checking in!",
-		href: 'https://github.com/PalmDevs/website',
-		image: Website,
-		hint: 'View source',
-	},
-	{
-		name: 'Git Bartender',
-		description:
-			'Providing Git shortcuts as well as a drinking problem and occasional yelling.',
-		href: 'https://github.com/PalmDevs/git-bartender',
-		image: GitBartender,
-		hint: 'View repository',
-	},
-	{
-		name: 'Jasper',
-		description: 'Providing Discord assistance as well as occasional yelling.',
-		href: 'https://github.com/PalmDevs/jasper-bot',
-		image: Jasper,
-		hint: 'View repository',
-	},
+	// {
+	// 	name: 'My Website',
+	// 	description: "You're on it right now. Thanks for checking in!",
+	// 	href: 'https://github.com/PalmDevs/website',
+	// 	image: Website,
+	// 	hint: 'View source',
+	// },
+	// {
+	// 	name: 'Git Bartender',
+	// 	description:
+	// 		'Providing Git shortcuts as well as a drinking problem and occasional yelling.',
+	// 	href: 'https://github.com/PalmDevs/git-bartender',
+	// 	image: GitBartender,
+	// 	hint: 'View repository',
+	// },
+	// {
+	// 	name: 'Jasper',
+	// 	description: 'Providing Discord assistance as well as occasional yelling.',
+	// 	href: 'https://github.com/PalmDevs/jasper-bot',
+	// 	image: Jasper,
+	// 	hint: 'View repository',
+	// },
 	{
 		name: 'Other projects',
 		description: 'Explore my other projects on GitHub.',
