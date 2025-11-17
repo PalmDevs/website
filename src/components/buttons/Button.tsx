040bf6a9606085b2634c261f5101a9c41c82aaf2
PalmDevs <git@palmdevs.me>
2025-11-17 23:34:18 +0700

    feat(components/buttons/Button): add `RadioButton` and `RadioButtonGroup`

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,9 +1,17 @@
-import { Show, splitProps } from 'solid-js'
+import {
+	createContext,
+	createEffect,
+	createSignal,
+	onCleanup,
+	Show,
+	splitProps,
+	useContext,
+} from 'solid-js'
 import { joinToString } from '~/utils/strings'
 import { IconComponentRenderer } from '../_icons'
 import Touchable from '../Touchable'
 import styles from './Button.module.css'
-import type { Component, JSX } from 'solid-js'
+import type { Accessor, Component, JSX, Setter } from 'solid-js'
 import type { IconComponent } from '../_icons'
 
 const defaultVariant: ButtonVariant = 'filled'
@@ -86,8 +94,114 @@ export const LinkButton: LinkButtonComponent = props => {
 	)
 }
 
+const RadioButtonGroupContext = createContext<{
+	name: string
+	selected: Accessor<string | undefined>
+	setSelected: Setter<string | undefined>
+}>()
+
+interface RadioButtonGroupProps<V extends string>
+	extends Omit<JSX.HTMLAttributes<HTMLFieldSetElement>, 'onChange'> {
+	name: string
+	default?: V
+	onChange?: (value: V | undefined) => void
+	children: JSX.Element
+}
+
+export const RadioButtonGroup = <V extends string>(
+	props: RadioButtonGroupProps<V>,
+) => {
+	const [selected, setSelected] = createSignal(props.default)
+	const [local, others] = splitProps(props, [
+		'name',
+		'onChange',
+		'default',
+		'children',
+	])
+
+	createEffect(() => {
+		local.onChange?.(selected())
+	})
+
+	return (
+		<RadioButtonGroupContext.Provider
+			value={{
+				name: local.name,
+				selected,
+				setSelected: setSelected as Setter<string | undefined>,
+			}}
+		>
+			<fieldset {...others}>{local.children}</fieldset>
+		</RadioButtonGroupContext.Provider>
+	)
+}
+
+export const RadioButton: RadioButtonComponent = props => {
+	const group = useContext(RadioButtonGroupContext)
+	const [local, others] = splitProps(props, [
+		'icon',
+		'class',
+		'text',
+		'trailingIcon',
+	])
+
+	const [radioProps] = splitProps(props, ['value'])
+	const [checked, setChecked] = createSignal(false)
+
+	createEffect(() => {
+		if (group) {
+			setChecked(group.selected() === radioProps.value)
+		}
+	})
+
+	// Only throw after all hooks used
+	if (!group)
+		throw new Error(
+			'RadioButton must be used within a RadioButtonGroup component',
+		)
+
+	return (
+		<>
+			<input
+				name={group.name}
+				type="radio"
+				checked={group.selected() === radioProps.value}
+				ref={radio => {
+					// Update with initial state
+					setChecked(radio.checked)
+
+					const update = () => group?.setSelected(radio.value)
+					radio.addEventListener('change', update)
+
+					onCleanup(() => {
+						radio.removeEventListener('change', update)
+					})
+				}}
+				{...radioProps}
+				id={others.for}
+				class={styles.radioInput}
+			/>
+			<Touchable
+				{...others}
+				as="label"
+				{...buttonProps(
+					checked() ? 'filled' : 'glass',
+					`${styles.radio} ${local.class ?? ''}`,
+				)}
+			>
+				<ButtonContentRenderer
+					text={local.text}
+					icon={local.icon}
+					trailingIcon={local.trailingIcon}
+				/>
+			</Touchable>
+		</>
+	)
+}
+
 type ButtonComponent = Component<ButtonProps>
 type LinkButtonComponent = Component<LinkButtonProps>
+type RadioButtonComponent = Component<RadioButtonProps>
 
 type ButtonVariant = 'filled' | 'glass' | 'outlined' | 'text' | 'text-tinted'
 
@@ -105,3 +219,11 @@ interface ButtonProps
 interface LinkButtonProps
 	extends ButtonOwnProps,
 		JSX.AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+interface RadioButtonProps
+	extends ButtonOwnProps,
+		JSX.LabelHTMLAttributes<HTMLLabelElement> {
+	variant?: never
+	for: string
+	value: string
+}
